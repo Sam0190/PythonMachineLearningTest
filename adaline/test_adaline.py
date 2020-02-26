d3a8073317872910b2dfcb7e8aa085b7581c1015
Elena Sollai <elena.sollai@brumbrum.it>
2020-02-26 11:07:53 +0100

    test_adaline

--- adaline/test_adaline.py
+++ adaline/test_adaline.py
@@ -3,26 +3,51 @@ import numpy as np
 import pandas as pd
 
 from adaline.AdalineGD import AdalineGD
+from perceptron.Perceptron import plot_decision_regions
 
-df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
-df.tail()
 
-y = df.iloc[0:100, 4].values
-y = np.where(y == 'Iris-setosa', -1, 1)
-X = df.iloc[0:100, [0, 2]].values
+def test_adaline():
 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+    df.tail()
 
-ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
-ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
-ax[0].set_xlabel('Epochs')
-ax[0].set_ylabel('log(Sum-squared-error)')
-ax[0].set_title('Adaline - Learning rate 0.01')
+    y = df.iloc[0:100, 4].values
+    y = np.where(y == 'Iris-setosa', -1, 1)
+    X = df.iloc[0:100, [0, 2]].values
 
-ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X,  y)
-ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
-ax[1].set_xlabel('Epochs')
-ax[1].set_ylabel('Sum-squared-error')
-ax[1].set_title('Adaline - Learning rate 0.0001')
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
 
-plt.show()
+    ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+    ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
+    ax[0].set_xlabel('Epochs')
+    ax[0].set_ylabel('log(Sum-squared-error)')
+    ax[0].set_title('Adaline - Learning rate 0.01')
+
+    ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X,  y)
+    ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+    ax[1].set_xlabel('Epochs')
+    ax[1].set_ylabel('Sum-squared-error')
+    ax[1].set_title('Adaline - Learning rate 0.0001')
+
+    plt.show()
+
+    X_std = np.copy(X)
+    X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+    X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+    ada = AdalineGD(n_iter=15, eta=0.01)
+    ada.fit(X_std, y)
+    plot_decision_regions(X_std, y, classifier=ada)
+    plt.title('Adaline - Gradient Descent')
+    plt.xlabel('sepal lenght [standardized]')
+    plt.ylabel('petal lenght [standardized]')
+    plt.legend(loc='upper left')
+    plt.show()
+    plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+    plt.xlabel('Epochs')
+    plt.ylabel('Sum-squared-error')
+    plt.show()
+
+
+if __name__ == '__main__':
+    test_adaline()
